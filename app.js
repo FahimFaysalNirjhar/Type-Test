d6c4c022d52b1840f386d89508f9c03f7242e854
Fahim Faysal Nirjhar <fahimfaysal1995@gmail.com>
2025-12-29 15:02:08 +0600

    word count and time count fuction added

--- app.js
+++ app.js
@@ -2,6 +2,15 @@ const URL = "https://meowfacts.herokuapp.com/?count=3";
 const para = document.querySelector("p");
 const textArea = document.querySelector("textarea");
 const btn = document.querySelector("button");
+let startTime, endTime;
+
+const wordCounter = () => {
+  let word = textArea.value.trim();
+  let Count = word.split(/[\s.,!?;:]+/).length;
+  console.log(Count);
+  return Count;
+};
+
 btn.addEventListener("click", () => {
   if (btn.innerText === "Start") {
     fetch(URL)
@@ -12,11 +21,21 @@ btn.addEventListener("click", () => {
         para.innerText = data.data;
         para.style.height = "10rem";
         para.style.overflow = "hidden";
+        textArea.removeAttribute("disabled");
+        btn.innerText = "Done";
+        textArea.addEventListener("focus", () => {
+          let time = new Date();
+          startTime = time.getTime();
+          console.log(startTime);
+        });
       });
-    textArea.removeAttribute("disabled");
-    btn.innerText = "Done";
   } else {
     btn.innerText = "Start";
     textArea.setAttribute("disabled", "true");
+    let time = new Date();
+    endTime = time.getTime();
+    let totalTime = (endTime - startTime) / 1000;
+    console.log(totalTime);
+    let wordCount = wordCounter();
   }
 });
